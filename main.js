ac7dc5358978b67d61aa6eb17d0383d64207d2d6
pavllychka bc2024-3 <spavlyk7@gmail.com>
2024-10-20 17:34:37 +0300

    Доробила 2 частину

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ program
   .requiredOption('-i, --input <path>', 'Path to input file') //обв'язково вказую шлх якй програма буде читати 
   .option('-o, --output <path>', 'Path to output file') // необов'язково вказую шлях в якому програма зберігає результат 
   .option('-d, --display', 'Display output to console'); // неоьов'язковий параметр який зберіга результат 
+  
 
 program.parse(process.argv); // аналіз 
 const options = program.opts(); // зберігає опції у змміній optipn 
@@ -24,16 +25,23 @@ if (!fs.existsSync(options.input)) {
 
 // читаю файл завдяки utf8 як текст 
 const data = fs.readFileSync(options.input, 'utf8');
+const jsonData = JSON.parse(data);
 
+const income = jsonData.find(item => item.txt === 'Доходи, усього');
+const expenses = jsonData.find(item => item.txt === 'Витрати, усього');
+
+const result = []; 
+if (income) {
+    result.push(`Доходи: ${income.value}`);
+}
+if (expenses) {
+    result.push (`Витрати: ${expenses.value}`);
+}
 
 if (options.output) {
-  fs.writeFileSync(options.output, data); // якщо output то файл 
+  fs.writeFileSync(options.output, result.join('\n'), 'utf8'); // якщо output то файл 
 }
 
 if (options.display) {
-  console.log(data); // якщо display то консоль
-}
-// якщо ні output, ні display не вказані  нічого не виводимо
-if (!options.output && !options.display) {
-  
+  console.log(result.join('\n')); // якщо display то консоль
 }
